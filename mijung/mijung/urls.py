b87d71952d63ede962e45aa78a761d27bd987ba6
holim0 <holim1226@gmail.com>
2019-11-20 19:48:21 +0900

    d

--- mijung/mijung/urls.py
+++ mijung/mijung/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import whateat.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', whateat.views.home, name='home'),
+    
 ]
